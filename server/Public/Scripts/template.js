ee7f63c49011b5852e6deef6fcff4e25e65f7616
iker <ikerkuadra@gmail.com>
2019-04-08 15:01:23 +0200

    Responsive v0.9

--- server/Public/Scripts/template.js
+++ server/Public/Scripts/template.js
@@ -4,20 +4,13 @@ x.ready(start);
 
 
 function start() {
-    let is_menu_drop = false;
+    let page_wdtd = $(document).width();
 
     //$('.dropmenu').click(show_menu);
+    console.log($(document).width());
 
 
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        console.log("Hey im on phone!");
-    }
-    /*let page_gg = $(window).resize(function (e) {
-
-        return e = $(document).width();
-    }).then();
-    console.log(page_gg);
-    if (page_gg <= 1000) {
+    if (page_wdtd <= 1000) {
         $('.nav_links').fadeOut();
         //    $('.icons').fadeOut();
         $('#responsive_menu').fadeIn();
@@ -71,12 +64,42 @@ function start() {
 
 
             }
-            
+
         }
 
+
+
         function next_destiny() {
+            console.log("entering2");
+            for (let z = 0; z < $('.Destiny').length; z++) {
+
+                if ($($('.Destiny')[z]).css('display') === 'block') {
+                    var destiny_to_change = $($('.Destiny')[z]).attr('id');
+                    $($('.Destiny')[z]).css('display', 'none');
+                    //    console.log(destiny_to_change)
 
 
+
+                    let wich_destiny = destiny_to_change.charAt(destiny_to_change.length - 1);
+                    //    console.log('Id separada' + wich_destiny);
+                    let new_destiny = 0;
+                    if (parseInt(wich_destiny) === 6) {
+                        new_destiny = 1;
+                    } else {
+                        new_destiny = ++wich_destiny;
+                    }
+                    //  console.log('Destiny_' + new_destiny);
+                    $('#Destiny_' + new_destiny).css('display', 'block');
+
+
+
+
+
+
+                }
+
+
+            }
 
         }
 
@@ -109,20 +132,134 @@ function start() {
 
 
 
-    console.log(page_gg);
+
+
+
+    /*   let page_gg = $(window).resize(function (e) {
+
+           return e = $(document).width();
+       }).then();
+       console.log(page_gg);
+       if (page_gg <= 1000) {
+           $('.nav_links').fadeOut();
+           //    $('.icons').fadeOut();
+           $('#responsive_menu').fadeIn();
+
+           transform_destiny();
+
+           function transform_destiny() {
+
+               for (let i = 1; i <= $('.Destiny').length; i++) {
+                   $($('.Destiny')[i]).css('display', 'none');
+
+
+               }
+           }
+
+
+
+           console.log('wtf is this shit');
+           $('.previous').click(previous_destiny);
+           $('.next').click(next_destiny);
+
+           function previous_destiny() {
+               console.log("entering");
+               // console.log($($('.Destiny')[0]).css('display'));
+               for (let z = 0; z < $('.Destiny').length; z++) {
+
+                   if ($($('.Destiny')[z]).css('display') === 'block') {
+                       var destiny_to_change = $($('.Destiny')[z]).attr('id');
+                       $($('.Destiny')[z]).css('display', 'none');
+                       //    console.log(destiny_to_change)
+
+
+
+                       let wich_destiny = destiny_to_change.charAt(destiny_to_change.length - 1);
+                       //    console.log('Id separada' + wich_destiny);
+                       let new_destiny = 0;
+                       if (parseInt(wich_destiny) === 1) {
+                           new_destiny = 6;
+                       } else {
+                           new_destiny = --wich_destiny;
+                       }
+                       //  console.log('Destiny_' + new_destiny);
+                       $('#Destiny_' + new_destiny).css('display', 'block');
 
 
 
 
 
-    //  $('#log_but').click(login);
-    // $('#register_link').click(show_register_form)
+
+                   }
+
+
+               }
+               
+           }
+
+           function next_destiny() {
+
+
+
+           }
 
 
 
 
 
 
+
+
+
+
+
+       } else {
+
+
+
+
+           $('.nav_links').fadeIn();
+           //    $('.icons').fadeIn();
+           $('#responsive_menu').fadeOut();
+
+
+           normalize_destiny();
+
+           function normalize_destiny() {
+               $('.Destiny').css('display', 'block');
+           }
+       }
+
+
+
+       console.log(page_gg);
+
+
+
+
+
+       //  $('#log_but').click(login);
+       // $('#register_link').click(show_register_form)
+
+
+
+
+
+
+
+
+
+       function show_menu() {
+           $('#myDropdown').fadeIn();
+           is_menu_drop = true;
+           $('.dropmenu').focusout(hide_menu);
+
+           function hide_menu() {
+               $('#myDropdown').fadeOut();
+           }
+
+       }
+       */
 
 
 
@@ -136,5 +273,4 @@ function start() {
         }
 
     }
-    */
 }
